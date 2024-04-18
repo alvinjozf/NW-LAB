b71b81d0c6365131afa9fff7b6a3f9407dd203b9
alvin joseph <159014896+alvinjozf@users.noreply.github.com>
2024-04-18 14:27:32 +0530

    Add files via upload

---
+++ String_1.java
@@ -0,0 +1,13 @@
+ class String {
+    public static void main(String args[])
+    {
+        String str="Java Programming";
+        System.out.println("the string is "+str+"");
+        System.out.println("Length of the string is"+str.length());
+        System.out.println("Character at second and forth position     is"+str.charAt(1)+","+str.charAt(3));
+        System.out.println("the sub string using start index only is "+str.substring(5));
+        System.out.println("the sub string using start index only is "+str.substring(0,4));
+
+
+    }
+    }
